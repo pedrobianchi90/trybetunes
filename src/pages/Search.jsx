fda052023caaa9894a8bd382b295ac1267c6fb32
Pedro Bianchi <pedroedm@gmail.com>
2022-03-07 19:47:52 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../components/Header';
 
-class Search extends React.Component {
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { artistName } = this.state;
+    const minLenght = 2;
+    const enabled = artistName.length >= minLenght;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <h2>Search</h2>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            name="artistName"
+            type="text"
+            value={ artistName }
+            onChange={ this.handleChange }
+            placeholder="Nome do Artista"
+          />
+        </form>
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ !enabled }
+        >
+          Procurar
+        </button>
       </div>
     );
   }
